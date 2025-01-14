e2cf8e724320a0a73e349bc0c15db7e8f45818a8
Serna Malala <sm.sernamalala@gmail.com>
2025-01-14 06:54:45 +0200

    created conversion functions

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,10 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (input) {
+
+  return Number((((input) - 32) * (5 / 9)).toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (input) {
+  return Number(((input * (9 / 5)) + 32).toFixed(1))
 };
 
 // Do not edit below this line
