ec463da782834f8eca45cb7e72b16b1bcb6517b2
Serna Malala <sm.sernamalala@gmail.com>
2025-01-14 06:30:28 +0200

    Updated function to sum up in both directions

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,9 +1,16 @@
 const sumAll = function (num1, num2) {
 
     let total = 0;
-    if (num1 >= 0 && num2 >= 0 && !isNaN(num1) && !isNaN(num2)) {
-        for (let i = num1; i <= num2; i++) {
-            total += i;
+    if (num1 >= 0 && num2 >= 0 && typeof num1 === "number" && typeof num2 === "number") {
+        if (num1 < num2) {
+            for (let i = num1; i <= num2; i++) {
+                total += i;
+            }
+        }
+        else {
+            for (let i = num1; i >= num2; i--) {
+                total += i;
+            }
         }
         return total;
     }
