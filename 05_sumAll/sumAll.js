8b43ce0c0ee47cae1a26e4c44c7fd00bb33eeab8
Serna Malala <sm.sernamalala@gmail.com>
2025-01-14 06:22:33 +0200

    made function sum up evrything

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,13 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
 
+    let total = 0;
+    for (let i = num1; i <= num2; i++) {
+        total += i;
+    }
+    return total;
 };
+
+console.log(sumAll(1, 4))
 
 // Do not edit below this line
 module.exports = sumAll;
