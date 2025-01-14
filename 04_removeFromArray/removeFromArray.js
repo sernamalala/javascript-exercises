c6d3a43168da5ba82f625db66bbe35723995134a
Serna Malala <sm.sernamalala@gmail.com>
2025-01-14 06:10:44 +0200

    remove from array with looping

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function (arrayInput, element) {
 
+    let newArray = [];
+    if (arrayInput.includes(element)) {
+        for (const singleElement of arrayInput) {
+            if (singleElement !== element) {
+                newArray.push(singleElement);
+            }
+        }
+    }
+    return newArray;
 };
-
+console.log(removeFromArray([1, 2, 3, 4], 3))
 // Do not edit below this line
 module.exports = removeFromArray;
