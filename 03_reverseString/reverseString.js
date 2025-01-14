d9b4e56ca96a98e48a392894fea6ec9225d4d82d
Serna Malala <sm.sernamalala@gmail.com>
2025-01-14 06:00:27 +0200

    Added a function to reverse a string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
+const reverseString = function (input) {
+
+    let newString = "";
+    for (let i = input.length - 1; i >= 0; i--) {
+        newString += input[i];
+    }
+    return newString;
 
 };
 
+//console.log(reverseString("Serna Malala"))
 // Do not edit below this line
 module.exports = reverseString;
