4c10469e396680ac828bc0c180b6e66273f92cb4
Serna Malala <sm.sernamalala@gmail.com>
2025-01-14 06:26:06 +0200

    added conditions for isNan and negative numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,10 +1,13 @@
 const sumAll = function (num1, num2) {
 
     let total = 0;
-    for (let i = num1; i <= num2; i++) {
-        total += i;
+    if (num1 >= 0 && num2 >= 0 && !isNaN(num1) && !isNaN(num2)) {
+        for (let i = num1; i <= num2; i++) {
+            total += i;
+        }
+        return total;
     }
-    return total;
+    return "ERROR";
 };
 
 console.log(sumAll(1, 4))
