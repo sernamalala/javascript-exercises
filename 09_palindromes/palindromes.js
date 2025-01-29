93983704b6dec160987f016e6b0cb4d532598dea
sernamalala <sm.sernamalala@gmail.com>
2025-01-29 23:42:33 +0200

    struggled with palidromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
+const palindromes = function (val) {
 
+    
+    let word = val.toLowerCase().replace(/[^a-z0-9]/g,"");
+
+    return word.split("").reverse().join("")==word;
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+// console.log(palindromes('serna'))
