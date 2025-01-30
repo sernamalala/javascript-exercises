2fb0de9fa435c9529f1a6dabafe326764c36f60a
sernamalala <sm.sernamalala@gmail.com>
2025-01-30 09:46:43 +0200

    created the get oldest age function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,42 @@
-const findTheOldest = function() {
+const findTheOldest = function(persons) {
+
+const date = new Date();
+
+    let ages = [];
+     persons.forEach((person) => {
+        let age = 0;
+        if(person.yearOfDeath){
+            age = person.yearOfDeath-person.yearOfBirth;
+        }
+        else{
+        age = date.getFullYear() - person.yearOfBirth;
+        }
+        ages.push(age);
+        
+     });
+     let maxAgeIndex = ages.indexOf(Math.max(...ages));
+     
+     return persons[maxAgeIndex];
 
 };
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
+//   const people = [
+//     {
+//       name: "Carly",
+//       yearOfBirth: 1066,
+//     },
+//     {
+//       name: "Ray",
+//       yearOfBirth: 1962,
+//       yearOfDeath: 2011,
+//     },
+//     {
+//       name: "Jane",
+//       yearOfBirth: 1912,
+//       yearOfDeath: 1941,
+//     },
+//   ]
+//   console.log(findTheOldest(people))
