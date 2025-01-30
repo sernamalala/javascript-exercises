878d418e1a34080e4f83e41366c2aa13af5ae8f5
sernamalala <sm.sernamalala@gmail.com>
2025-01-30 09:11:21 +0200

    completed the fibonacci code

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
 
+    let arr = [0,1];
+   
+    if(num<0){
+        return "OOPS"
+    }
+    else if(num===0){
+        return 0
+    }
+    else if(num===1){
+        return 1
+    }
+    for (let index = 0; index < num; index++) {
+        let nextVal = arr[arr.length - 1] + arr[arr.length - 2];
+        arr.push(nextVal)
+        
+    }
+
+    return arr[num]
+    
 };
 
 // Do not edit below this line
