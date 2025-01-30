f75776e9c07c30f4865138af60dd7dcd83d64b71
sernamalala <sm.sernamalala@gmail.com>
2025-01-30 09:19:01 +0200

    created the function that extracts the book titles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,24 @@
-const getTheTitles = function() {
+const getTheTitles = function(arr) {
+let newArr = [];
+    arr.forEach(element => {
 
+        newArr.push(element.title)
+        
+    });
+
+    return newArr
 };
 
 // Do not edit below this line
 module.exports = getTheTitles;
+// const books = [
+//     {
+//       title: 'Book',
+//       author: 'Name'
+//     },
+//     {
+//       title: 'Book2',
+//       author: 'Name2'
+//     }
+//   ]
+// console.log(getTheTitles(books))
